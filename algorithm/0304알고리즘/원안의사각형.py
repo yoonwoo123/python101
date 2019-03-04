02eec68d17653e1957414f0bf506655f5c28e6fd
YoonWoo <lkkjasd@korea.ac.kr>
2019-03-04 23:57:44 +0900

    0304집

--- algorithm/0304알고리즘/원안의사각형.py
+++ algorithm/0304알고리즘/원안의사각형.py
@@ -2,17 +2,17 @@ import sys
 sys.stdin = open("원안의사각형_input.txt")
 
 R = int(input())
-s = int(R // 1.414)
 
-int((R // 1.414)**2) +
+root = 1.414
+s = int((R * 0.414) //root)
 
-while h > 1:
-    w = s + 1
-    h = s - 1
-    total += 2*(w * h - s * h)
-    s = w
-    h = w
-# if w < h:
-#     total += (w * h - s * w)
+total = 0
+
 # 사분면을 4개로 나눠서 생각한다치고 사분면 1개 * 4 해주자
 
+total = (R * R) - (s+1)**2
+
+# 범위는 1부터 R - s
+for i in range(1, R-s):
+    total -= 2 * (int((((i**2 + R**2)**0.5 - R) // root)) + 1)
+print(4*total)
