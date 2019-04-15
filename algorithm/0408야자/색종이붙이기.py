b107bf27da854673eb39c69f712125f487c1ad7f
Yoon-Young-Woo <rlatmf2147@naver.com>
2019-04-15 20:00:33 +0900

    0415|색종이

--- algorithm/0408야자/색종이붙이기.py
+++ algorithm/0408야자/색종이붙이기.py
@@ -1,17 +1,32 @@
 import sys, copy
 sys.stdin = open("색종이붙이기_input.txt")
 
+def find(o):
+    for x in range(N - o):
+        for y in range(N - o):
+            flag = 0
+            for i in range(x, x + o + 1):
+                if flag == 1: break
+                for j in range(y, y + o + 1):
+                    if arr[i][j] == 0:
+                        flag = 1
+                        break
+            if flag == 0:
+                cnt[o] += 1
+                for k in range(x, x + o + 1):
+                    for t in range(y, y + o + 1):
+                        arr[k][t] = 0
+
+
 arr = []
 N = 10
-
 for i in range(N):
     arr.append(list(map(int, input().split())))
-
 ori = copy.deepcopy(arr)
-
 # for g in arr:
 #     print(g)
 # print()
+
 
 a = 0
 my_min = 99999999999
@@ -22,20 +37,8 @@ for u in range(4, 0, -1):
     cnt = [0] * 5
 
     for o in range(u, -1, -1):
-        for x in range(N - o):
-            for y in range(N - o):
-                flag = 0
-                for i in range(x, x + o + 1):
-                    if flag == 1: break
-                    for j in range(y, y + o + 1):
-                        if arr[i][j] == 0:
-                            flag = 1
-                            break
-                if flag == 0:
-                    cnt[o] += 1
-                    for k in range(x, x + o + 1):
-                        for t in range(y, y + o + 1):
-                            arr[k][t] = 0
+        find(o)
+
     # print(cnt)
     if sum(cnt) < my_min:
         for i in range(5):
---
+++ 정리/0415 django 로그인 로그아웃.md
@@ -0,0 +1,32 @@
+# 0415 django 로그인 로그아웃
+
+** 디버깅 퀘스트 ( 금 ) 에 있는데 다른 사람의 프로젝트를 받아서 쓰는 경우 제일 먼저 해야
+
+​     해야 하는 일은 db가 없을 수 있으므로 makemigrations 와 migrate를 먼저 해봐야한다.
+
+
+
+*** 과목평가
+
+Login require post 안 내고 순수 ORM ( 메소드, sql 슬라이드ppt파일 (create, save, insert 등)) 출제했다,  auto-now 중요
+
+
+
+ 우선 POST CRUD 잘 되는지 확인해보자
+
+
+
+accounts app 부터 만들고 하자.
+
+1. 회원가입
+
+   U, D 로그인 로그아웃
+
+2.  댓글 생성
+
+   
+
+3. 1:N > User - Comment, Post   /  Post - Comment
+
+
+
